8b6373d42326d863b2eb067edefeee72bf84a07d
srinathdev-techjays <srinath.a@techjays.com>
2024-05-04 18:48:53 +0530

    Initial Commit

---
+++ Swift-UI-Intermediate/Swift_UI_IntermediateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_IntermediateApp.swift
+//  Swift-UI-Intermediate
+//
+//  Created by Srinath Dev on 04/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_IntermediateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
