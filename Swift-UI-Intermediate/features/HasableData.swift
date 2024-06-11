dd283dd7e54f4aff4d763d2045a543e180df9d01
srinathdev-techjays <srinath.a@techjays.com>
2024-06-12 02:11:40 +0530

    Background Process added.

---
+++ Swift-UI-Intermediate/features/HasableData.swift
@@ -0,0 +1,18 @@
+//
+//  HasableData.swift
+//  Swift-UI-Intermediate
+//
+//  Created by Srinath Dev on 10/06/24.
+//
+
+import SwiftUI
+
+struct HasableData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HasableData()
+}
